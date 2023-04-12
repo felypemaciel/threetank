47d0dcefc5d202d3bf736f1fee4395b5e0bc1b5c
maciel <felypemaciel674@gmail.com>
2023-04-12 20:00:20 -0300

    unotank sims and simulink

--- unotank.m
+++ unotank.m
@@ -6,33 +6,58 @@
 close all;
 
 % model parameters
-S = 4.513E-3;       % tank cross sectional area (m2)
-Sp = 2.507E-5;      % inter tank cross sectional area (m2)
+r = 6.8/2;
+S = pi*r*r;       % tank cross sectional area (cm2)
+Sp = 0.2507;      % inter tank cross sectional area (cm2)
 
 mu = 0.5;           % outflow coefficients
 mu20 = 0.675;
 
-qmax = 3.34E-5;     % maximum flowrate (m3/s)
-lmax = 0.26;        % maximum level (m)
+qmax = 33.4;     % maximum flowrate (cm3/s)
+lmax = 25;        % maximum level (cm)
 
-g = 9.8;            % gravity (m/s2)
+g = 981;            % gravity (cm/s2)
 
 % inputs
-qin = 3.5E-5;       % pump 1 flow (m3/s)
-qout = 0.8E-4;
+qin = 35;       % pump 1 flow (cm3/s)
 
 % initial conditions
-x0 = 0.05;           % first tank
+x0 = 2;           % first tank (cm)
 
 trange = 0:0.1:5000;      % time range
 
 % nonlinear model
-[t, xnl] = ode45(@(t,x)nonlinear1tank(t,x,S,qin,mu20,Sp,g), trange, x0);
+[t, xnl] = ode45(@(t,x)nonlinear1tank(t,x,S,10,mu20,Sp,g), trange, x0);
 
 plot(trange,xnl)
+title('Open Loop')
+xlabel('time (s)')
+ylabel('height (cm)')
+
+xss = fsolve(@(x)nonlinear1tank(t, x, qin, S, mu20, Sp, g),x0);
+
+syms x1 u1 g mu20 S Sp 
+q20 = mu20*Sp*sqrt(2*g*x1);
+f = 1/S*(u1 - q20);
+x = x1;
+u = u1;
+
+A_ss = jacobian(f,x);
+B_ss = jacobian(f,u);
+
+S = pi*r*r;       % tank cross sectional area (cm2)
+Sp = 0.2507;      % inter tank cross sectional area (cm2)
+mu20 = 0.675;
+g = 981;            % gravity (cm/s2)
+x1 = xss;
+u1 = 35;
+
+A = eval(A_ss);
+B = eval(B_ss);
+C = 1;
+D= 0;
 
 function dxdt = nonlinear1tank(t,x,S,qin,mu20,Sp,g)
     q20 = mu20*Sp*sqrt(2*g*x(1));
     dxdt = 1/S*(qin - q20);
-
 end
